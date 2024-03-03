43bc527e2b14c873a0407e3ce3543bf8c872b04b
Khalifa Idris <ibrahim.idris.developer@gmail.com>
2024-03-03 16:44:19 +0000

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Khalifa Idris on 03/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
